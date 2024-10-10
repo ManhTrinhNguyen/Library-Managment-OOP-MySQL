dddd0abe8e5e3ff7dc6fd1d869033d2e1a65b7dd
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2024-10-10 12:24:09 -0700

    Add borrow class

---
+++ borrow.py
@@ -0,0 +1,51 @@
+from database import DATABASE 
+
+class Borrow: 
+  def __init__(self, db):
+    self.db_connection = db 
+
+  # Allow member to borrow mutilple books (one to many relationship)
+  def borrow_book(self, member_id, book_ids):
+    # book_ids should be a list
+    # Iterate through book_ids
+    for book_id in book_ids:
+      # Check if book available 
+      query_book_stock='SELECT stock FROM books WHERE book_id=%s'
+      self.db_connection.cursor.execute(query_book_stock, (book_id,))
+      book_stock = self.db_connection.cursor.fetchone()
+      if book_stock and book_stock[0] > 0:
+         # Insert into borrows table 
+         query_borrow_book='INSERT INTO borrows(member_id, book_id) VALUES(%s, %s)'
+         self.db_connection.cursor.execute(query_borrow_book, (member_id, book_id))
+
+         # Reduce stock 
+         reduce_stock = book_stock[0] - 1
+         query_reduce_stock='UPDATE books SET stock=%s WHERE book_id=%s'
+         self.db_connection.cursor.execute(query_reduce_stock,(reduce_stock, book_id))
+      else:
+        return f'Book with {book_id} is out of stock'
+    # Commit Transaction
+    self.db_connection.db.commit()
+    print(f"Member {member_id} successfully borrowed books: {book_ids}")
+
+  def return_book(self, book_id):
+    # Return book 
+    query_return='UPDATE borrows SET return_date=CURRENT_TIMESTAMP WHERE book_id=%s'
+    self.db_connection.cursor.execute(query_return, (book_id,))
+
+    # Update sock in books
+    query_book_stock='SELECT stock FROM books WHERE book_id=%s'
+    self.db_connection.cursor.execute(query_book_stock, (book_id,))
+    book_stock = self.db_connection.cursor.fetchone()[0]
+    new_stock = book_stock + 1
+    query_update_book_stock='UPDATE books SET stock=%s WHERE book_id=%s'
+    self.db_connection.cursor.execute(query_update_book_stock, (new_stock, book_id))
+
+    self.db_connection.db.commit()
+
+
+
+
+borrow_1 = Borrow(DATABASE())
+#borrow_1.borrow_book(2, [3,4])
+borrow_1.return_book(3)
